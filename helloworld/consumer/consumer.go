da9e2d9051bf8145cb72d2fda74733a5f0c967bf
dxyan06 <dxyan06@gmail.com>
2021-09-10 14:55:42 +0800

    update

--- helloworld/consumer/consumer.go
+++ helloworld/consumer/consumer.go
@@ -15,11 +15,14 @@ func Consume(topic string) {
 		log.Fatal("NewConsumer err: ", err)
 	}
 	defer consumer.Close()
+
+    // 如果 consumer 优先启动，那么 consumer 将会漏掉 producer 已经推送的消息，很可能什么都读不到
 	partitionConsumer, err := consumer.ConsumePartition(topic, 0, sarama.OffsetNewest)
 	if err != nil {
 		log.Fatal("ConsumePartition err: ", err)
 	}
 	defer partitionConsumer.Close()
+
 	for message := range partitionConsumer.Messages() {
 		log.Printf("[Consumer] partitionid: %d; offset:%d, value: %s\n", message.Partition, message.Offset, string(message.Value))
 	}
