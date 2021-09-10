a6b68268f0f0952dca9d335a48dcbec8936761eb
dxyan06 <dxyan06@gmail.com>
2021-09-10 15:10:06 +0800

    update

--- producer/async/cmd/main.go
+++ producer/async/cmd/main.go
@@ -10,8 +10,11 @@ import (
 
 func main() {
 	topic := conf.Topic
+    // 消费者从偏移位置 0 开始消费消息
 	go standalone.SinglePartition(topic)
 	time.Sleep(time.Millisecond * 100) // 延迟，让consumer启动后再启动生产者
+
+    // 异步生产消息
 	async.Producer(topic, 100)
 
 	time.Sleep(time.Second * 10)
--- producer/sync/cmd/main.go
+++ producer/sync/cmd/main.go
@@ -11,9 +11,14 @@ import (
 func main() {
 	topic := conf.Topic
 	// 先启动消费者,保证能消费到后续发送的消息
+    // 因为 sarama.OffsetOldest 配置，消费者总是从消费位移0开始
+    // 消费成功以后，kafka不会清理消息，消费者位移决定读取位置
 	go standalone.SinglePartition(topic)
 	time.Sleep(time.Second)
+
+    // 向该 topic 发送 100 get消息
 	sync.Producer(topic, 100)
+
 	// sleep 等待消费结束
 	time.Sleep(time.Second * 10)
 }
